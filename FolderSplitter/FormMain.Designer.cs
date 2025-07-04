d5204c8b587c77ce3b78c81917f5ac66bbcd0ad0
Vitaliy <mitradis@gmail.com>
2025-07-04 14:07:32 +0300

    Upload

---
+++ FolderSplitter/FormMain.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace FolderSplitter
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button3 = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.folderBrowserDialog2 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(301, 7);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 30);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Целевая папка";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Enabled = false;
+            this.button2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(301, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(153, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Папка назначения";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.numericUpDown1.Location = new System.Drawing.Point(302, 79);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            -1981284352,
+            -1966660860,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(151, 22);
+            this.numericUpDown1.TabIndex = 5;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            1073741824,
+            0,
+            0,
+            0});
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // button3
+            // 
+            this.button3.Enabled = false;
+            this.button3.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(7, 145);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(447, 36);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Разделить";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(8, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(287, 20);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Максимальный размер папки в байтах:";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(301, 102);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(153, 20);
+            this.label4.TabIndex = 7;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(301, 122);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(153, 20);
+            this.label5.TabIndex = 8;
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(8, 11);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox1.Size = new System.Drawing.Size(287, 22);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBox2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(8, 47);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Horizontal;
+            this.textBox2.Size = new System.Drawing.Size(287, 22);
+            this.textBox2.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(8, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(287, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "в мегабайтах:";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Arial", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(8, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(287, 20);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "в гигабайтах:";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // FormMain
+            // 
+            this.ClientSize = new System.Drawing.Size(461, 188);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Folder Splitter";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+    }
+}
+
---
+++ FolderSplitter/FormMain.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace FolderSplitter
+{
+    public partial class FormMain : Form
+    {
+        string folderStart = null;
+        string folderDest = null;
+        string startDirectoryName = null;
+        long sizeFilesInFilder = 0;
+        int numberFolder = 1;
+
+        public FormMain()
+        {
+            InitializeComponent();
+            numericUpDown1_ValueChanged(this, new EventArgs());
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog1.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DialogResult result = folderBrowserDialog1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                folderStart = folderBrowserDialog1.SelectedPath;
+                startDirectoryName = new DirectoryInfo(folderStart).Name;
+                textBox1.Text = folderStart;
+                button2.Enabled = true;
+                button3.Enabled = false;
+                folderDest = null;
+                textBox2.Clear();
+            }
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            folderBrowserDialog2.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            DialogResult result = folderBrowserDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                textBox2.Text = folderDestFunction();
+                button3.Enabled = true;
+            }
+        }
+
+        void button3_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(folderStart))
+            {
+                enableDisable(false);
+                folderDest = folderDestFunction();
+                searchAllForders(folderStart);
+                enableDisable(true);
+                sizeFilesInFilder = 0;
+                numberFolder = 1;
+            }
+        }
+
+        void searchAllForders(string startLocation)
+        {
+            if (Directory.Exists(startLocation))
+            {
+                moveFiles(startLocation);
+                foreach (var directory in Directory.GetDirectories(startLocation))
+                {
+                    searchAllForders(directory);
+                }
+            }
+        }
+
+        void moveFiles(string PathScan)
+        {
+            string dirName = PathScan.Remove(0, folderStart.Length);
+            foreach (var line in Directory.EnumerateFiles(PathScan))
+            {
+                if (sizeFilesInFilder >= numericUpDown1.Value)
+                {
+                    sizeFilesInFilder = 0;
+                    numberFolder += 1;
+                    folderDest = folderDestFunction();
+                }
+                string fileName = line.Remove(0, folderStart.Length);
+                if (!Directory.Exists(folderDest + dirName))
+                {
+                    try
+                    {
+                        Directory.CreateDirectory(folderDest + dirName);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Не удалось создать папку: " + folderDest + dirName + Environment.NewLine + ex.Message);
+                    }
+                }
+                try
+                {
+                    long fileSize = new FileInfo(line).Length;
+                    sizeFilesInFilder = sizeFilesInFilder + fileSize;
+                    File.Move(line, folderDest + fileName);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Не удалось переместить: " + line + Environment.NewLine + ex.Message);
+                }
+            }
+        }
+
+        void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            label4.Text = (numericUpDown1.Value / 1024 / 1024).ToString("F") + " MB";
+            label5.Text = (numericUpDown1.Value / 1024 / 1024 / 1024).ToString("F") + " GB";
+        }
+
+        string folderDestFunction()
+        {
+            return Path.Combine(folderBrowserDialog2.SelectedPath, startDirectoryName + "_" + numberFolder.ToString());
+        }
+
+        void enableDisable(bool enable)
+        {
+            button1.Enabled = enable;
+            button2.Enabled = enable;
+            button3.Enabled = enable;
+        }
+    }
+}
